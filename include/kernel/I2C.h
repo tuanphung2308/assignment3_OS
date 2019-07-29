d72a41d4c89e19ef9d8e18ba39614787402241c4
athena2308 <tuan2308>
2019-07-29 15:40:07 +0700

    I2C header

--- include/kernel/I2C.h
+++ include/kernel/I2C.h
@@ -5,15 +5,33 @@
 
 enum
 {
-    I2C_BASE    = (0x3F205000),         // I2C Base address
-    I2C_C       = (I2C_BASE + 0x00),    // I2C Control 
-    I2C_s       = (I2C_BASE + 0x04),    // I2C Status
-    I2C_DLEN    = (I2C_BASE + 0x08),    // I2C Data Length
-    I2C_A       = (I2C_BASE + 0x0c),    // I2C Slave Address 
-    I2C_FIFO    = (I2C_BASE + 0x10),    // I2C Data FIFO 
-    I2C_DIV     = (I2C_BASE + 0x14),    // I2C Clock Divider
-    I2C_DEL     = (I2C_BASE + 0x18),    // I2C Data Delay
-    I2C_CLKT    = (I2C_BASE + 0x1c),    // I2C Clock Stretch Timeout 
+    I2C_MASTER_BASE    = (0x3F205000),         // I2C Base address
+    I2C_MASTER_C       = (I2C_MASTER_BASE + 0x00),    // I2C Control 
+    I2C_MASTER_S       = (I2C_MASTER_BASE + 0x04),    // I2C Status
+    I2C_MASTER_DLEN    = (I2C_MASTER_BASE + 0x08),    // I2C Data Length
+    I2C_MASTER_A       = (I2C_MASTER_BASE + 0x0c),    // I2C Slave Address 
+    I2C_MASTER_FIFO    = (I2C_MASTER_BASE + 0x10),    // I2C Data FIFO 
+    I2C_MASTER_DIV     = (I2C_MASTER_BASE + 0x14),    // I2C Clock Divider
+    I2C_MASTER_DEL     = (I2C_MASTER_BASE + 0x18),    // I2C Data Delay
+    I2C_MASTER_CLKT    = (I2C_MASTER_BASE + 0x1c),    // I2C Clock Stretch Timeout 
+
+    I2C_SLAVE_BASE = (0x7E214000), //BSC SLAVE BASE
+    I2C_SLAVE_DR = (I2C_SLAVE_BASE + 0x00), //DATA REGISTER
+    I2C_SLAVE_RSR = (I2C_SLAVE_BASE + 0x04), //STATUS REGISTER AND ERROR CLEAR REGISTER
+    I2C_SLAVE_SLV = (I2C_SLAVE_BASE + 0x08), // I2C slave address value 
+    I2C_SLAVE_CR = (I2C_SLAVE_BASE + 0x0C), // Control register 
+    I2C_SLAVE_FR = (I2C_SLAVE_BASE + 0x10), //Flag register
+    I2C_SLAVE_IFLS = (I2C_SLAVE_BASE + 0x14), //Interrupt fifo level select register
+    I2C_SLAVE_IMSC = (I2C_SLAVE_BASE + 0x18), //Interupt Mask Set Clear Register 
+    I2C_SLAVE_RIS = (I2C_SLAVE_BASE + 0x1c), //Raw Interupt Status Register 
+    I2C_SLAVE_MIS = (I2C_SLAVE_BASE + 0x20), //Masked Interupt Status Register 
+    I2C_SLAVE_ICR = (I2C_SLAVE_BASE + 0x24), //Interupt Clear Register 
+    I2C_SLAVE_DMACR = (I2C_SLAVE_BASE + 0x28), //DMA Control Register 
+    I2C_SLAVE_TDR = (I2C_SLAVE_BASE + 0x2C), //FIFO Test Data 
+    I2C_SLAVE_GPUSTAT = (I2C_SLAVE_BASE + 0x30), //GPU Status Register 
+    I2C_SLAVE_HCTRL = (I2C_SLAVE_BASE + 0x34), //Host Control Register 
+    I2C_SLAVE_DEBUG1 = (I2C_SLAVE_BASE + 0x38), //I2C Debug Register
+    I2C_SLAVE_DEBUG2 = (I2C_SLAVE_BASE + 0x3C), //SPI Debug Register 
 };
 
 typedef union I2C_flags
